424acd5074104c7ef3761fbcfc1f70678e863da6
sseritan <sseritan@gmail.com>
2019-07-15 14:30:38 -0700

    Partition code updated to use Eppstein's IntegerPartitions.py

--- generate_set.py
+++ generate_set.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
-from collections_extended import bag
+from collections_extended import bag # multiset implementation
 import random
 import sys
+
+from IntegerPartitions import partitions
+
 
 def generate_set(N, K):
 	""" Generate sets of numbers for number partitioning problem
@@ -12,33 +15,12 @@ def generate_set(N, K):
 		K (int): Total sum
 	"""
 	if N < 2:
-		raise RuntimeError('Should be more than 2 numbers')
+		raise RuntimeError('Should be more than 2 numbers for set split')
 
-	# Initialize sets with subset totals
-	setA = bag([K // 2])
-	setB = bag([K // 2 + K%2])
-
-
-	# Go through and subdivide one set
-	while len(setA) + len(setB) != N:
-		# Randomly pick a number in a set
-		target_set = random.choice([setA, setB])
-		target_number = random.choice(list(target_set))
-
-		try:
-			new_el = random.randint(1, target_number-1)
-			remainder = target_number - new_el
-
-			# Subset sum logic
-			print('Sets before change: {} {}'.format(setA, setB))
-			target_set.remove(target_number)
-			target_set.add(new_el)
-			target_set.add(remainder)
-			print('Sets after change: {} {}'.format(setA, setB))
-		except:
-			continue
-
-	return setA, setB
+	for p in partitions(K):
+		for q in partitions(K):
+			if len(p) + len(q) == K:
+				print("Allowed partition: {} {}".format(p, q))
 
 if __name__ == '__main__':
 	if len(sys.argv) != 3:
@@ -47,6 +29,4 @@ if __name__ == '__main__':
 	N = int(sys.argv[1])
 	K = int(sys.argv[2])
 
-	A, B = generate_set(N, K)
-	print('Set A: {} (total: {})'.format(A, sum(A)))
-	print('Set B: {} (total: {})'.format(B, sum(B)))
+	generate_set(N, K)
